d2e0869b5f19f0911f34d4b21e9256c47640c54d
JackyCCC <jacky9813@hotmail.com>
2017-08-28 21:51:42 +0800

    Misinclude the upgrade js file/ minor changes for upgrader

--- scripts/dbUpgrade.js
+++ scripts/dbUpgrade.js
@@ -1,5 +1,5 @@
 function pso2cDBUpgrade(pso2c,fromVersion=""){
-    console.log("Upgrading from " + fromVersion +" to "+ pso2c.version);
+    console.log("Upgrading from " + (fromVersion==""?"unknown":fromVersion) +" to "+ pso2c.version);
     switch(fromVersion){
         case "":
         case "0.0.1-alpha":
@@ -8,8 +8,9 @@ function pso2cDBUpgrade(pso2c,fromVersion=""){
                 `version` TEXT NULL,\
                 `__ignore_this` INTEGER PRIMARY KEY DEFAULT 1 CHECK(__ignore_this=1) \
             )",(e,r)=>{
-                pso2c._db.run("INSERT INTO pso2Tools(version) VALUES (?)",[this.version]);
+                pso2c._db.run("INSERT INTO pso2Tools(version) VALUES (?)",[pso2c.version]);
             });
             
     }
+    pso2c._db.run("UPDATE pso2Tools SET version=?",[pso2c.version],(e,r)=>{return;});
 }
