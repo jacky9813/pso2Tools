9f78232f598a653d33fc02c308c458646cb0a15e
JackyCCC <jacky9813@hotmail.com>
2018-06-13 10:34:01 +0800

    database upgrade will operate when launch, minor changes to package.json

--- index.js
+++ index.js
@@ -11,6 +11,52 @@ const BrowserWindow = electron.BrowserWindow;
 
 const path = require("path");
 const url = require("url");
+const fs = require("fs");
+
+var oldFile = "pso2Tools.db";
+var newFile = "pso2db.db";
+
+function upgradeDatabase(dbFile){
+    var db = require("sqlite-sync");
+    var offset = (new Date().getTimezoneOffset())*60;
+    var timeahead = offset<0;
+    offset = Math.abs(offset);
+    db.connect(dbFile)
+    var commands = [
+        "CREATE TABLE temp_table AS SELECT * FROM characters",
+        "DROP TABLE characters",
+        "CREATE TABLE IF NOT EXISTS `characters`(\
+            `cid` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,\
+            `CName` TEXT,\
+            `lastTATime` TIMESTAMP,\
+            `classLv` TEXT,\
+            `memo` TEXT\
+        );",
+        "INSERT INTO characters(CName, lastTATime, classLv, memo) SELECT CName, strftime('%s',lastTATime)"+ (timeahead?"-":"+") + offset.toString() +", classLv, memo FROM temp_table",
+        "DROP TABLE temp_table",
+        "CREATE TABLE IF NOT EXISTS `installed_modules`(`module_name` TEXT PRIMARY KEY);",
+        "INSERT INTO installed_modules(module_name) VALUES ('ClassLevel')",
+        "INSERT INTO installed_modules(module_name) VALUES ('Memo')",
+        "INSERT INTO installed_modules(module_name) VALUES ('TaCounter')",
+    ]
+    for(var cmd in commands){
+        console.log(db.run(commands[cmd]));
+    }
+}
+
+if(!path.isAbsolute(oldFile)){
+    oldFile = path.join(process.cwd(),oldFile);
+}
+
+if(!path.isAbsolute(newFile)){
+    newFile = path.join(process.cwd(),newFile);
+}
+
+if(fs.existsSync(oldFile)){
+    fs.rename(oldFile, newFile, ()=>{upgradeDatabase(newFile);})
+}else{
+    upgradeDatabase(newFile);
+}
 
 let mainWindow;
 
