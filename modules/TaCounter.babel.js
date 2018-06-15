b565e1c93314daec7f1a434911fe38b7a4b78080
JackyCCC <jacky9813@hotmail.com>
2018-06-15 16:18:14 +0800

    Red background is always better with white foreground

--- modules/TaCounter.babel.js
+++ modules/TaCounter.babel.js
@@ -52,7 +52,7 @@ class TaCounter extends pso2tools_module{
         var lastTaTime = this.getTATime(cid);
         var cdComplete = Date.now() > (lastTaTime+ (3600*166))*1000
         return (<div>{this.langList.PrevTATime+(new Date(lastTaTime*1000)).toLocaleString()}<br/>
-        <span style={{background: (cdComplete?"unset":"red"), color: (cdComplete?"green":"unset")}}>{this.langList.CDCompleteTime+(new Date(lastTaTime*1000+ (1000*3600*166))).toLocaleString()}</span><br />
+        <span style={{background: (cdComplete?"unset":"red"), color: (cdComplete?"green":"white")}}>{this.langList.CDCompleteTime+(new Date(lastTaTime*1000+ (1000*3600*166))).toLocaleString()}</span><br />
         <TaCounterSetTime cid={cid} type={"Now"} label={this.langList.JustDone} click={()=>{this.setTATime(cid,null)}}/><br/>
         <TaCounterDateSelector cid={cid} /><br/>
         <TaCounterSetTime cid={cid} type={"CDComplete"} label={this.langList.CDDoneAt} click={()=>{this.setTATime(cid,(new Date(document.querySelector("#TaCounterDateSel"+cid.toString()).value)).getTime()/1000-(3600*166))}} />
@@ -61,6 +61,6 @@ class TaCounter extends pso2tools_module{
 
     renderCharTable(cid){
         var TACDtime = this.getTATime(cid)*1000+1000*3600*166;  // Chrome timestamp
-        return <td key={"tbltaCounter"+cid.toString()}>{(Date.now() > TACDtime?(<span style={{color:"green"}}>{this.langList.TAMissionAvailable}</span>):(<span style={{background:"red"}}>{this.langList.CDUntil+((new Date(TACDtime)).toLocaleString())}</span>))}</td>;
+        return <td key={"tbltaCounter"+cid.toString()}>{(Date.now() > TACDtime?(<span style={{color:"green"}}>{this.langList.TAMissionAvailable}</span>):(<span style={{background:"red",color:"white"}}>{this.langList.CDUntil+((new Date(TACDtime)).toLocaleString())}</span>))}</td>;
     }
 }
